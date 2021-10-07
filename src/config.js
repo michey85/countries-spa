84ff81698fee1c0dbff81fa7c5290cef869c60f4
Mikhail Nepomnyashchiy <nem@kodix.ru>
2021-10-07 09:35:33 +0300

    removed api key

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 const BASE_URL = 'https://restcountries.com/v2/';
-const API_KEY = process.env.REACT_APP_API_KEY;
-
 
 export const ALL_COUNTRIES = BASE_URL + 'all?fields=name,capital,flags,population,region,alpha3Code';
-export const searchByCountry = (name) => BASE_URL + 'name/' + name + '?access_key=' + API_KEY;
-export const filterByRegion = (region) => BASE_URL + 'region/' + region + '?access_key=' + API_KEY;
+export const searchByCountry = (name) => BASE_URL + 'name/' + name;
+export const filterByRegion = (region) => BASE_URL + 'region/' + region;
